535baeb18f08ed043dbe963eff3220396e9fa27a
Maks <maks.tk95@gmail.com>
2024-07-24 20:54:12 +0200

    repair of leaked content

--- src/components/LineSVG/LineSVG.jsx
+++ src/components/LineSVG/LineSVG.jsx
@@ -2,7 +2,7 @@ import './LineSVG.scss'
 
 export function LineSVG() {
 	return (
-		<svg viewBox='0 0 100 100' preserveAspectRatio='none'>
+		<svg className='line' viewBox='0 0 100 100' preserveAspectRatio='none'>
 			<path d='M0,100 Q50,50 100,0' />
 		</svg>
 	)
